ae3148d833fbac9a76a23d7e9f03466041f4903d
glory6807 <pjj2818@gmail.com>
2021-10-31 15:40:31 +0900

    controller 생성, 로그인 버튼 생성

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,12 @@ import { AppController } from './app.controller';
 import { Querybuilder } from './querybuilder/querybuilder';
 import { BoardService } from './board/board.service';
 import { BoardController } from './board/board.controller';
+import { FreeController } from './free/free.controller';
+import { FreeService } from './free/free.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, BoardController],
-  providers: [Querybuilder, BoardService],
+  controllers: [AppController, BoardController, FreeController],
+  providers: [Querybuilder, BoardService, FreeService],
 })
 export class AppModule {}
