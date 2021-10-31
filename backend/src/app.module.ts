d6fc3e04d1311d48a0008842405cf633d8e05519
ywc412 <ywc412@gmail.com>
2021-10-31 15:43:56 +0900

    Auth Controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,11 @@ import { AppController } from './app.controller';
 import { Querybuilder } from './querybuilder/querybuilder';
 import { BoardService } from './board/board.service';
 import { BoardController } from './board/board.controller';
+import { AuthController } from './auth/auth.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, BoardController],
+  controllers: [AppController, BoardController, AuthController],
   providers: [Querybuilder, BoardService],
 })
 export class AppModule {}
