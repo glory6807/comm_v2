216a5ee9cf4c862d95c793ac28e42c1eda691b04
glory6807 <pjj2818@gmail.com>
2021-11-16 22:37:51 +0900

    로그인 로직 controller에서 service로 옮김

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Param } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { map } from 'rxjs';
 import { Querybuilder } from 'src/querybuilder/querybuilder';
 
 @Injectable()
@@ -18,4 +20,27 @@ export class AuthService {
 
         return await this.dao.insert("auth", "selectUserById", sqlParam);
     }
+
+    async getNaverProfile(@Param('naverToken') naverToken :string) {
+        let httpservice = new HttpService();
+        let url = "https://openapi.naver.com/v1/nid/me";
+
+        let naverProfile = await httpservice.post(url, {}, {
+                    headers : {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer ' + naverToken
+                    }
+               }).toPromise();
+        
+        let profileParam = {
+            'id': naverProfile.data.response.id,
+            'nickname': naverProfile.data.response.nickname,
+            'email': naverProfile.data.response.email,
+            'regType':'naver'
+        }
+
+        return await this.dao.insert("auth", "selectUserById", profileParam);
+    }
+
+
 }
