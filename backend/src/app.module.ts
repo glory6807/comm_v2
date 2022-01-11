8813a794dde68048df6b317f774fb6d00699aa27
ywc412 <ywc412@gmail.com>
2022-01-11 22:42:35 +0900

    commit

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -29,6 +29,6 @@ import { AnalyzeSchedulerService } from './schedule/analyze-scheduler.service';
 })
 export class AppModule implements NestModule{
   configure(consumer: MiddlewareConsumer){
-    consumer.apply(AuthCheckMiddleware).forRoutes(MemberController);
+    //consumer.apply(AuthCheckMiddleware).forRoutes(MemberController);
   }
 }
